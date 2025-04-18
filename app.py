56c2159f8ba0c1bbcbde7c4ca2223002a7d2fff4
Andre Mezavila <mezavilaandre@yahoo.com.br>
2025-04-17 21:43:50 -0300

    add second route

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from data import students
 
 app = Flask(__name__)
 
-@app.route("/students", methods=['GET'] )
-def get():
+@app.route('/students', methods=['GET'] )
+def get_students_all():
     return students
 
+@app.route('/students/<int:student_id>', methods=['GET'])
+def get_student_id(student_id):
+  return students.get(
+      student_id,
+      {'error': 'Student not found'}
+  )
