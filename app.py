ac29c0a5928df6503e57f7e2c0e41c485805fa57
Andre Mezavila <mezavilaandre@yahoo.com.br>
2025-04-17 23:37:13 -0300

    add post route !

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, request  
 from data import students
 
 app = Flask(__name__)
 
 @app.route('/students', methods=['GET'] )
-def get_students_all():
+def list_students_all():
     return students
 
 @app.route('/students/<int:student_id>', methods=['GET'])
-def get_student_id(student_id):
+def list_student_id(student_id):
   return students.get(
       student_id,
       {'error': 'Student not found'}
   )
+
+  @app.route('/students', methods=['POST'])
+  def create_student():
+    id = sorted(students.keys())[-1] +1
+    new_student = {
+        'name' : request.json['name'],
+        'email' : request.json['email'],
+        'room' : request.json['room'],
+    }
+    students[id] = new_student
+    return new_student
