602b9903fe19b63af1f9bfcc04004a8f44a00aa9
Andre Mezavila <mezavilaandre@yahoo.com.br>
2025-04-22 12:59:27 -0300

    finished project with simple file

--- app.py
+++ app.py
@@ -25,19 +25,6 @@ def create_student():
     students[id] = new_student
     return new_student
 
-@app.route('/students/<int:student_id>', methods=['PUT'])
-def update_student(student_id):
-    if student_id not in students:
-        return {'error': 'Student not found'}, 404
-
-    updated_student = {
-        'name': request.json.get('name', students[student_id]['name']),
-        'email': request.json.get('email', students[student_id]['email']),
-        'room': request.json.get('room', students[student_id]['room'])
-    }
-    students[student_id] = updated_student
-    return updated_student
-
 @app.route('/students/<int:student_id>', methods=['DELETE'])
 def delete_student(student_id):
     if student_id not in students:
@@ -46,3 +33,12 @@ def delete_student(student_id):
     del students[student_id]
     return {'message': 'Student deleted successfully'}
 
+@app.route('/students/<int:student_id>', methods=['PUT'])
+def update_student(student_id):
+    if student_id in students.keys():
+        students[student_id]['name']=request.json['name']
+        students[student_id]['email']=request.json['email']
+        students[student_id]['room']=request.json['room']
+        return students[student_id]
+    else:
+        return {'error': 'student not found'}
